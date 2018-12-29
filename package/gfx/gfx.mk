a2943f6944d0ce8169b17e13abbc7f4934155b5e
Erico Nunes <nunes.erico@gmail.com>
2018-12-29 20:05:38 +0100

    gfx: build all gbm-surface samples
    Signed-off-by: Erico Nunes <nunes.erico@gmail.com>

--- package/gfx/gfx.mk
+++ package/gfx/gfx.mk
@@ -10,24 +10,30 @@ GFX_LICENSE = Unknown
 GFX_LICENSE_FILES = .gitignore
 GFX_DEPENDENCIES = mesa3d libdrm libpng
 
+GFX_GBM_SURFACE_LIST = gbm-surface \
+	gbm-surface-blend \
+	gbm-surface-clear \
+	gbm-surface-color \
+	gbm-surface-draw \
+	gbm-surface-fbo \
+	gbm-surface-gbm-fbo \
+	gbm-surface-index \
+	gbm-surface-line \
+	gbm-surface-move \
+	gbm-surface-part \
+	gbm-surface-render \
+	gbm-surface-render-two \
+	gbm-surface-scissor \
+	gbm-surface-tex
+
 # Makefile in the repo uses host 'gcc' directly.
 # This below uses a make implicit rule so that it picks variables like CC.
 define GFX_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/gbm-surface main LDLIBS="-lgbm -lepoxy -lpng"
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/gbm-surface-color main LDLIBS="-lgbm -lepoxy -lpng"
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/gbm-surface-move main LDLIBS="-lgbm -lepoxy -lpng"
+	for i in $(GFX_GBM_SURFACE_LIST); do $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/$$i main LDLIBS="-lgbm -lepoxy -lpng"; done
 endef
 
 define GFX_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/local/gfx/gbm-surface
-	cp -v $(@D)/gbm-surface/main $(TARGET_DIR)/usr/local/gfx/gbm-surface/gbm-surface
-	cp -v $(@D)/gbm-surface/*.glsl $(TARGET_DIR)/usr/local/gfx/gbm-surface/
-	mkdir -p $(TARGET_DIR)/usr/local/gfx/gbm-surface-color
-	cp -v $(@D)/gbm-surface-color/main $(TARGET_DIR)/usr/local/gfx/gbm-surface-color/gbm-surface-color
-	cp -v $(@D)/gbm-surface-color/*.glsl $(TARGET_DIR)/usr/local/gfx/gbm-surface-color/
-	mkdir -p $(TARGET_DIR)/usr/local/gfx/gbm-surface-move
-	cp -v $(@D)/gbm-surface-move/main $(TARGET_DIR)/usr/local/gfx/gbm-surface-move/gbm-surface-move
-	cp -v $(@D)/gbm-surface-move/*.glsl $(TARGET_DIR)/usr/local/gfx/gbm-surface-move/
+	for i in $(GFX_GBM_SURFACE_LIST); do mkdir -p $(TARGET_DIR)/usr/local/gfx/$$i; cp -v $(@D)/$$i/main $(TARGET_DIR)/usr/local/gfx/$$i/$$i; cp -v $(@D)/$$i/*.{glsl,png} $(TARGET_DIR)/usr/local/gfx/$$i/; done
 endef
 
 $(eval $(generic-package))
